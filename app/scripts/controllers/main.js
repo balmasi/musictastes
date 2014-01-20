4df6405ee2fc99159041d814898c3714a4b22bc0
Borna <borna@borna-almasi.com>
2014-01-20 04:25:18 -0500

    Now fully functional prototype

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,14 +13,16 @@ app.controller('MainCtrl', function ($scope) {
 
 
 app.controller('authenticationCtrl', [
-    '$scope',
-    '$timeout',
-    'Facebook',
-    function($scope, $timeout, Facebook) {
-      
+  '$scope',
+  '$timeout',
+  '$q',
+  'Facebook',
+
+  function($scope, $timeout, $q, Facebook) {
+
       // Define user empty data :/
       $scope.user = {};
-      
+      $scope.top10 = [null,null,null,null,null,null,null,null,null,null];
       // Defining user logged status
       $scope.logged = false;
       
@@ -32,7 +34,7 @@ app.controller('authenticationCtrl', [
        * Watch for Facebook to be ready.
        * There's also the event that could be used
        */
-      $scope.$watch(
+       $scope.$watch(
         function() {
           return Facebook.isReady();
         },
@@ -40,15 +42,17 @@ app.controller('authenticationCtrl', [
           if (newVal)
             $scope.facebookReady = true;
         }
-      );
-      
+        );
+
       /**
        * IntentLogin
        */
-      $scope.IntentLogin = function() {
+       $scope.IntentLogin = function() {
         Facebook.getLoginStatus(function(response) {
+          console.log("response is:" , response)
           if (response.status == 'connected') {
             $scope.logged = true;
+            $scope.authToken = response.authResponse.accessToken
             $scope.me(); 
           }
           else
@@ -65,7 +69,10 @@ app.controller('authenticationCtrl', [
             $scope.logged = true;
             $scope.me();
           }
-        
+
+        },
+        {
+          scope: 'user_likes,user_friends,friends_likes'
         });
        };
        
@@ -77,29 +84,104 @@ app.controller('authenticationCtrl', [
             /**
              * Using $scope.$apply since this happens outside angular framework.
              */
-            $scope.$apply(function() {
+             $scope.$apply(function() {
               $scope.user = response;
             });
-            
-          });
+
+           });
         };
-      
+
       /**
        * Logout
        */
-      $scope.logout = function() {
+       $scope.logout = function() {
         Facebook.logout(function() {
           $scope.$apply(function() {
             $scope.user   = {};
             $scope.logged = false;  
           });
         });
-      }
+      };
       
+
+
+      $scope.getMyLikes = function () {
+        Facebook.api('/me/music?limit=200', function(response){
+          $scope.$apply(function(){
+            $scope.myMusic = {};
+            var data = response.data;
+
+            for (var i=0;i<data.length; i++){
+              var curArtist = data[i];
+              $scope.myMusic[curArtist.id] = {name: curArtist.name}
+            }
+
+            $scope.myMusicFetched = true;
+          });
+        });
+      };
+
+
+      var setMusicData = function (key) {
+          Facebook.api('/'+key+'/music?limit=200',function(response){
+            var data = response.data;
+            var common = $scope.friends[key].common;
+            for (var i= 0; i < data.length; i++) {
+              var artist = data[i];
+              // If we share interest in artist, add to common
+              if ($scope.myMusic[artist.id]){
+                common.push([artist.id,artist.name]);
+              }
+            }
+            for (var j=0;j<10;j++){
+                if ($scope.top10[j] === null || common.length > $scope.top10[j].common.length) {
+                  $scope.top10[j] = {
+                    friend_id: key,
+                    friend_name: $scope.friends[key].name,
+                    friend_pic: $scope.friends[key].pic,
+                    common: common
+                  };
+                  break;
+                }
+            }
+        });
+      }
+      // Get list of all friends with non-empty music likes
+      //SELECT uid,name FROM user WHERE uid in (SELECT uid1 from friend WHERE uid2 = me()) AND music <> ""
+
+      // Get list of all music my friends like
+      //   SELECT page_id, name FROM page WHERE page_id IN (
+      // SELECT page_id FROM page_fan WHERE uid in (SELECT uid1 from friend WHERE uid2 = me()) AND profile_section="music")
+      $scope.getFriendsLikes = function () {
+        $scope.getMyLikes();
+        Facebook.api({
+            method: 'fql.query',
+            query: 'SELECT uid,name, pic_square FROM user WHERE uid in (SELECT uid1 from friend WHERE uid2 = me()) AND music <> "" LIMIT 100'
+        },
+        function(response){
+          $scope.$apply(function(){
+            $scope.friends = {};
+            // Get all my friends in an id -> name hash
+            for (var i=0;i<response.length -1; i++){
+              var curFriend = response[i];
+              $scope.friends[curFriend.uid] = {
+                name: curFriend.name,
+                pic: curFriend.pic_square,
+                common: [] }
+            }
+            
+            // Find Friend's likes
+            for (var key in $scope.friends) {
+             setMusicData(key);
+           }
+
+         });
+        });
+      };
       /**
        * Taking approach of Events :D
        */
-      $scope.$on('Facebook:statusChange', function(ev, data) {
+       $scope.$on('Facebook:statusChange', function(ev, data) {
         console.log('Status: ', data);
         if (data.status == 'connected') {
           $scope.$apply(function() {
@@ -120,7 +202,29 @@ app.controller('authenticationCtrl', [
         
         
       });
-      
-      
+
+
+     }
+     ])
+
+  /**
+   * Just for debugging purposes.
+   * Shows objects in a pretty way
+   */
+   .directive('debug', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        expression: '=val'
+      },
+      template: '<pre>{{debug(expression)}}</pre>',
+      link: function(scope) {
+        // pretty-prints
+        scope.debug = function(exp) {
+          return angular.toJson(exp, true);
+        };
+      }
     }
-  ]);
+  })
+
+   ;
